b8129a0e318823b720522f64ad6e1c83ba4eda6c
Avalak <avalak@users.noreply.github.com>
2026-07-06 01:33:44 +0400

    fix: suppress script-runner exit on plugin not found in built-in dir

--- src/script-runner.sh
+++ src/script-runner.sh
@@ -22,15 +22,13 @@ EXTERNAL_DIR="/opt/yt-dlp-jailed/scripts-external"
 source /opt/yt-dlp-jailed/lib/plugins.sh
 
 # Built-in scripts
-script_path=$(find_plugin "${CMD}" "${SCRIPTS_DIR}")
-if [ -n "${script_path}" ]; then
+if script_path=$(find_plugin "${CMD}" "${SCRIPTS_DIR}"); then
     execute_plugin "${script_path}" "${@}"
 fi
 
 # External scripts
 if [ -d "${EXTERNAL_DIR}" ]; then
-    script_path=$(find_plugin "${CMD}" "${EXTERNAL_DIR}")
-    if [ -n "${script_path}" ]; then
+    if script_path=$(find_plugin "${CMD}" "${EXTERNAL_DIR}"); then
         execute_plugin "${script_path}" "${@}"
     fi
 fi
